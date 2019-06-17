f08c065e682b1f44e821ceaf3a3503e229956c59
damianc <damianczapiewski@gmail.com>
2019-06-17 23:42:18 +0200

    Change heading tag to dynamic one

--- vue-with-jsx/H1Component.js
+++ vue-with-jsx/H1Component.js
@@ -2,33 +2,12 @@ import Vue from 'vue';
 
 export default Vue.extend({
 	render: function (h) {
+    	var HTag = `h${this.level}`;
     	var content = this.$slots.default;
-    	var heading;
-    	
-    	switch (+this.level) {
-    		case 1:
-    			heading = <h1>{content}</h1>;
-    			break;
-    		case 2:
-    			heading = <h2>{content}</h2>;
-    			break;
-    		case 3:
-    			heading = <h3>{content}</h3>;
-    			break;
-    		case 4:
-    			heading = <h4>{content}</h4>;
-    			break;
-    		case 5:
-    			heading = <h5>{content}</h5>;
-    			break;
-    		case 6:
-    			heading = <h6>{content}</h6>;
-    			break;
-    	}
 
     	return (
     		<div>
-	    		{heading}
+	    		<HTag>{content}</HTag>
     		</div>
     	);
   	},
